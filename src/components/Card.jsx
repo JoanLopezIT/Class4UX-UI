fe56b0960ce9b036ba27f062aa385288d63c0d38
YagoGu <yago.gutierrez15@gmail.com>
2023-08-24 17:43:27 +0200

    css

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,16 +2,13 @@ import ProductPage from "../pages/ProductPage";
 
 function Card(props) {
     const { name, image, description, id } = props.product;
-
-
-
     return (
-        <div className="card">
+        <>
             <img src={image} />
             <p>{name}</p>
             <p>Description: {description}</p>
             <button onClick={() => (id)}>Add Cart</button>
-        </div>
+        </>
     )
 }
 
