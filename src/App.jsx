ca2724c70027712447950b74a9347067fc5e4691
German Panzardi <gerpanza@gmail.com>
2023-08-24 16:10:24 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,29 @@
 import './App.css'
 import { Routes, Route } from "react-router-dom";
 
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import ProductPage from './pages/ProductPage';
+
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-
+      <Header />
       <Routes>
         {/* <Route  path="/" element={ <HomePage /> } /> */}
 
-        <Route path="/" element />
-        <Route path="/about" element />
-
-        <Route
-          path="/projects"
-          element
-        />
+        <Route path="/" element={<ProductPage />} />
 
         <Route
           path="/products/:id"
           element
         />
 
-
-
         <Route path="*" element />
       </Routes>
+      <Footer />
     </div>
   )
 }
