cd5e538673117b4f91a8b8df758176736a132379
YagoGu <yago.gutierrez15@gmail.com>
2023-08-24 18:01:21 +0200

    more css

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,6 +1,6 @@
 import { useParams, Link } from 'react-router-dom'
 import productJson from "../data.json";
-
+import "./ProductDetail.css"
 
 
 function ProductDetail(props) {
@@ -17,10 +17,14 @@ function ProductDetail(props) {
     return (
         <div className='card-detail'>
             <img src={product.image} />
-            <p>{product.name}</p>
-            <p>Description: {product.description}</p>
-            <button onClick={() => handleCard()}>Add Cart</button>
-           <Link to={`/cart`}><button onClick={() => handleCard()}>Buy</button></Link>
+            <div className='desc'>
+                <p>{product.name}</p>
+                <p>Description: {product.description}</p>
+                <div className="btn-group">
+                    <button onClick={() => handleCard()}>Add Cart</button>
+                    <Link to={`/cart`}><button onClick={() => handleCard()}>Buy</button></Link>
+                </div>
+            </div>
         </div>
     )
 }
